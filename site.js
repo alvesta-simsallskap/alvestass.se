6cec7cba79e5c84335173fe284e4500a1f149951
Johan Marand <johan@leafcode.se>
2025-09-29 11:50:03 +0200

    Changed strategy. Single page simple app with pages cms for content

---
+++ site.js
@@ -0,0 +1,14 @@
+
+/*
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollToPlugin);
+});
+
+function scrollToHash(hash, e) {
+  const elem = hash ? document.querySelector(hash) : false;
+  if (elem) {
+    if (e) e.preventDefault();
+    gsap.to(window, { duration: 1, scrollTo: { y: elem, offsetY: 20 } });
+  }
+}
+*/
