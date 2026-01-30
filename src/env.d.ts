23b8a6890a990df78064ceafa5bfd03827b0f758
Johan Marand <johan.marand@aveki.se>
2026-01-30 23:47:45 +0100

    extend the Env type definition

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,10 +3,11 @@ type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
 declare namespace App {
   interface Locals extends Runtime {
     runtime: {
-      env: Env;
-    };
-    otherLocals: {
-      YOUR_ENV_KEY: string;
-    };
+      env: {
+        MJ_APIKEY_PUBLIC: string;
+        MJ_APIKEY_PRIVATE: string;
+        TURNSTILE_SECRET_KEY: string;
+      };
+    }
   }
 }
