fbff97f6e70c9da1c430e03071b18d6be13b232a
Johan Marand <johan.marand@aveki.se>
2026-02-03 19:27:45 +0100

    Time as hh:mm in email

--- src/pages/api/send-time-report.ts
+++ src/pages/api/send-time-report.ts
@@ -60,7 +60,7 @@ export const POST: APIRoute = async ({ request, locals }) => {
         } else if (item.h === 10) {
           time = 'Halvdag';
         } else {
-          time = `${item.h} h ${item.m} m`;
+          time = `${item.h}:${item.m}`;
         }
         
         rows += `<tr><td>${val}</td><td>${time}</td></tr>`;
@@ -68,7 +68,7 @@ export const POST: APIRoute = async ({ request, locals }) => {
         rows += `<tr><td>${val}</td><td></td></tr>`;
       }
     }
-    return `<h4>${label}</h4><table border="1" cellpadding="4" style="border-collapse:collapse;margin-bottom:1em;"><thead><tr><th>Datum & aktivitet</th><th>Tid</th></tr></thead><tbody>${rows}</tbody></table>`;
+    return `<h4>${label}</h4><table border="1" cellpadding="4" style="border-collapse:collapse;margin-bottom:1em;"><thead><tr><th>Datum och aktivitet</th><th>Tid</th></tr></thead><tbody>${rows}</tbody></table>`;
   }
 
   // Compose email content (HTML)
